aac1b709459c3af6c6213c8662be5d3c58080959
Ana Santos <17-10602@usb.ve>
2024-06-13 01:09:32 -0400

    ADD: Endpoints GET to search with string and PUT to update product

--- entregas/Grupo06/index.js
+++ entregas/Grupo06/index.js
@@ -29,32 +29,89 @@ app.get("/", (req, res) => {
 
 //Endpoint GET para obtener todas las computadoras
 app.get("/computadoras", async (req, res) => {
-    res.status(500).send("NO IMPLEMENTADO");
+    res.status(501).send("NO IMPLEMENTADO");
 });
 
 //Endpoint GET para obtener una computadora por codigo
 app.get("/computadoras/:codigo", async (req, res) => {
-    res.status(500).send("NO IMPLEMENTADO");
+    res.status(501).send("NO IMPLEMENTADO");
 });
 
-//Endpoint GET para obtener todas las computadoras por nombre o descripcion
-app.get("/computadoras/search", async (req, res) => {
-    res.status(500).send("NO IMPLEMENTADO");
+//Endpoint GET para obtener todas las computadoras por nombre o descripcion (categoria)
+app.get("/computadoras/search/:search", async (req, res) => {
+    const search = req.params.search;
+    const client = await connectToMongoDB();
+
+    if (!client) {
+        res.status(503).send('Error al conectar con la base de datos')
+        return;
+    }
+
+    const regex = new RegExp(search.toLowerCase(), 'i'); 
+    const db = client.db('Grupo06')
+    const computadoras = await db.collection('computadoras')
+        .find({ $or: [{ nombre: regex }, { categoria: regex }] }).toArray()
+    await disconnectToMongoDB()
+
+    computadoras.length == 0 ? res.status(404).send("No se encontraron computadoras con el nombre o categoria "+ search)
+        : res.status(200).json(computadoras)
+    
 });
 
 //Endpoint POST para agregar una computadora
 app.post("/computadoras", async (req, res) => {
-    res.status(500).send("NO IMPLEMENTADO");
+    res.status(501).send("NO IMPLEMENTADO");
 });
 
 //Endpoint PUT para modificar una computadora
 app.put("/computadoras/:codigo", async (req, res) => {
-    res.status(500).send("NO IMPLEMENTADO");
+    const codigo = req.params.codigo;
+    const update = req.body;
+    console.log(update);
+
+    if (Object.keys(update).length === 0) {
+        res.status(400).send("Falta el cuerpo del mensaje");
+        return;
+    }
+
+    const client = await connectToMongoDB();
+    if (!client) {
+        res.status(503).send("Error al conectar con la base de datos");
+        return;
+    }
+
+    const db = client.db('Grupo06');
+    const computadoras = await db.collection('computadoras')
+    const computadoraEncontrada = await computadoras.findOne({ codigo: parseInt(codigo) });
+
+    console.log(computadoraEncontrada);
+    if (!computadoraEncontrada) {
+        res.status(404).send("No se encontro la computadora con codigo "+ codigo);
+        return;
+    }
+
+    //Incluye solo los campos del objeto que existen en la base de datos
+    for (const campos in update) {
+        if (!computadoraEncontrada.hasOwnProperty(campos)) {
+            res.status(500).send(`Error al actualizar el producto, campo ${campos} no encontrado en la base de datos`)
+            return;
+        }
+    }
+
+    computadoras.updateOne({ codigo: parseInt(codigo) }, { $set: update }).then(() => {
+        console.log(`Producto con codigo ${codigo} actualizado correctamente.`)
+        res.status(201).send(update);
+    }).catch((error) => {
+        console.error("Error al actualizar el producto", error)
+        res.status(500).send("Error al actualizar el producto")
+    }).finally(async () => {
+        await disconnectToMongoDB()
+    });
 });
 
 //Endpoint DELETE para eliminar una computadora 
 app.delete("/computadoras/:codigo", async (req, res) => {
-    res.status(500).send("NO IMPLEMENTADO");
+    res.status(501).send("NO IMPLEMENTADO");
 });
 
 
@@ -66,3 +123,11 @@ app.get("*", (req, res) => {
     });
   });
   
+/*      GLOSARIO DE ERRORES
+200 OK: Respuesta estándar para solicitudes correctas.
+201 Created: La solicitud ha tenido éxito y se ha creado o autualizado recurso.
+400 Bad Request: La solicitud contiene sintaxis incorrecta o no puede procesarse.
+404 Not Found: El servidor no pudo encontrar el contenido solicitado.
+501 Not Implemented: La solicitud no se ha implementado.
+503 Service Unavailable: El servidor no está disponible.
+*/
